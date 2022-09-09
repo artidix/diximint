bde2d5d558331d74d4c65d004817931ea6bbf36f
Albert Garuda <algarud@gmail.com>
2022-09-09 23:55:33 +0300

    react-router deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main } from "./Main";
+import { About } from "./About";
 
 function App() {
   return (
     <div>
       <header>
         <Button variant="contained">Just Web3 it!</Button>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
