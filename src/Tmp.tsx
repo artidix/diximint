695e730110147f4c228de7e482002c343d203284
Albert Garuda <algarud@gmail.com>
2022-09-15 00:17:06 +0300

    profile menu cleanups

--- src/Tmp.tsx
+++ src/Tmp.tsx
@@ -1,20 +1,6 @@
 import React from 'react'
-import { Menu, MenuItem, Divider, Button } from '@mui/material'
+import { Menu, MenuItem, Divider, Button, Box } from '@mui/material'
 import LogoutIcon from '@mui/icons-material/Logout';
-
-const styles = {
-  menu: {
-    backgroundColor: 'brown',
-    border: '1px blue solid'
-  },
-  item: {
-    backgroundColor: 'yellow',
-  },
-  icon: {
-    margin: '1rem .5rem .5rem .5rem',
-    border: '1px lime solid'
-  }
-}
 
 export const Tmp = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -26,7 +12,6 @@ export const Tmp = () => {
     setAnchorEl(null);
   };
 
-
   return (
     <React.Fragment>
       <Button onClick={handleClick} variant='contained'>Menu</Button>
@@ -35,12 +20,10 @@ export const Tmp = () => {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        style={styles.menu}
       >
         {/* <Divider sx={{ my: 0.1 }} /> */}
-        <MenuItem style={styles.item} onClick={handleClose} disableRipple>
-          <LogoutIcon style={styles.icon} />
-          Disconnect
+        <MenuItem onClick={handleClose} >
+          <Button startIcon={<LogoutIcon />}>Disconnect</Button>
         </MenuItem>
       </Menu>
     </React.Fragment>
