f1a9c92c9a76bcbefa6714232b6ff308e66fc6a1
Albert Garuda <algarud@gmail.com>
2022-09-19 15:14:37 +0300

    minting part

--- src/features/mint/MintButton.tsx
+++ src/features/mint/MintButton.tsx
@@ -4,11 +4,23 @@ import { useSnackbar } from "notistack";
 import { mintThunk, MintThunkInput } from "./mintTunk";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { currentPriceSelector } from "./mintSlice";
+import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { CONTRACT_ADDRESS } from "../../common/app.config";
+import { abi } from "../../common/chainclient";
+
 
 export const MintButton = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const dispatch = useAppDispatch();
   const phrase = useAppSelector(currentPriceSelector);
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: abi,
+    functionName: 'mintItem',
+  })
+
+  const { write } = useContractWrite(config)
 
   
   const requestMinting = async (_phrase: string) => {
@@ -27,7 +39,9 @@ export const MintButton = () => {
       sx={{ margin: '.5rem', width: '10rem' }}
       disabled={phrase == ''}
       startIcon={<RocketLaunchIcon />}
-      onClick={() => requestMinting(phrase)}>
+      // onClick={() => requestMinting(phrase)}
+      onClick={() => write?.()}
+      >
       Mint
     </Button>
   )
