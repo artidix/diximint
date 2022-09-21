aebbc8cf5bb842a755723d3c3824a80f54fe352a
Albert Garuda <algarud@gmail.com>
2022-09-21 10:36:05 +0300

    styling images

--- src/common/ImageBox.tsx
+++ src/common/ImageBox.tsx
@@ -1,12 +1,11 @@
 import { Avatar, Box } from "@mui/material"
 
 export const ImageBox = ({img, label} : {img: string, label?: string}) => {
+  const maxWidth = 400;
 
   return (
-    <Box display='flex' flexDirection='row' justifyContent='center' margin='1rem' marginBottom='2rem'>
+    <Box display='flex' flexDirection='row' justifyContent='center' margin='1rem' marginBottom='2rem' maxWidth='300px'>
       <img alt="Sample" src={'img/' + img} style={{
-        // width: 200,
-        // height: 200,
         maxWidth: '100%',
         height: 'auto'
       }} />
