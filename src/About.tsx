19d5fec5dfa8e81b08cac924a6f5f26dd7120c62
Albert Garuda <algarud@gmail.com>
2022-09-22 21:29:39 +0300

    centered box

--- src/About.tsx
+++ src/About.tsx
@@ -27,33 +27,39 @@ const StyledInsiderBlock = styled(Box)`
   padding: 1rem;
 `
 
+const CenteredRowBox = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
 export const About = () => {
   return (
     <StyledRoot>
       <StyledPaper>
         <StyledInsiderBlock>
-          <Box display='flex' flexDirection='row' justifyContent='center'>
+          <Box>
             🦓
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center' marginTop='.5rem'>
+          <Box marginTop='.5rem'>
             Mint your unique NFT created by AI
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center'>
+          <Box>
             Then challenge others to guess it!
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center'>
+          <Box>
             <ImageBox img='artidix_logo2.png' />
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center'>
+          <Box>
             Type what do you want to see on the picture then just Mint it.
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center' marginBottom='.5rem'>
+          <Box marginBottom='.5rem'>
             Every next NFT costs more. Hurry up while it's cheap!
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center'>
+          <Box>
             <Button href='/'>🥁 Try 🥁</Button>
           </Box>
-          <Box display='flex' flexDirection='row' justifyContent='center' marginTop='.5rem'>
+          <Box marginTop='.5rem'>
             Later you will be able to join the Game: share link to your image and challenge others to guess what was your initial text phrase. The closest winner takes over your NFT and gets ability to mint a new one for free to continue The Game. But you win the money if they don't guess it right 😉
           </Box>
           <Box display='flex' flexDirection='row' justifyContent='center' marginTop='1rem'>
