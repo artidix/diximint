afded433c31b20ea977360ff9ac7a5ed01a4b4e5
Albert Garuda <algarud@gmail.com>
2022-09-23 22:41:02 +0300

    for mint ethers direct call

--- src/features/mint/MintButton.tsx
+++ src/features/mint/MintButton.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react"
 import { Button } from "@mui/material"
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
+import { ethers } from 'ethers'
+import { abi } from "../../common/chainclient"
+import { CONTRACT_ADDRESS } from "../../common/app.config"
+declare let window: any
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+const signer = provider.getSigner();
+const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
 export const MintButton = ({ phrase }: { phrase: string }) => {
   const [isSuccess, setSuccess] = useState(false);
@@ -9,7 +17,10 @@ export const MintButton = ({ phrase }: { phrase: string }) => {
   const phraseHash = 'QmRAQB6YaCyidP37UdDnjFY5vQuiBrcqdyoW1CuDgwxkD4';
 
   async function handleMint() {
-    
+    console.log('mmintz', phrase);
+    console.log('signer', signer);
+    console.log(signer._address);
+
 
   }
 
