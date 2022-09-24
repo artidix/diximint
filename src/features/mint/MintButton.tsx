29d1a5bce4535d5cccbe5f8ffe571facd24e3eff
Albert Garuda <algarud@gmail.com>
2022-09-24 14:53:13 +0300

    on minting use price from state

--- src/features/mint/MintButton.tsx
+++ src/features/mint/MintButton.tsx
@@ -4,6 +4,8 @@ import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
 import { ethers } from 'ethers'
 import { abi } from "../../common/chainclient"
 import { CONTRACT_ADDRESS } from "../../common/app.config"
+import { useAppSelector } from "../../hooks"
+import { priceSelector } from "../price/priceSlice"
 declare let window: any
 
 const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
@@ -11,13 +13,17 @@ const signer = provider.getSigner();
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
 export const MintButton = ({ phrase }: { phrase: string }) => {
+  const currentPrice = useAppSelector(priceSelector);
   const [isSuccess, setSuccess] = useState(false);
 
   async function handleMint() {
     const hashArr = ethers.utils.hashMessage(phrase);
 
+    if (currentPrice == null)
+      throw ("Minting Price cannot be null");
+
     const tx = await contract.mintItem(hashArr, false, {
-      value: ethers.utils.parseEther("0.1")
+      value: ethers.utils.parseEther(currentPrice)
     });
 
     console.log('play.tx:', tx);
@@ -30,7 +36,7 @@ export const MintButton = ({ phrase }: { phrase: string }) => {
       variant="contained"
       color="secondary"
       sx={{ margin: '.5rem', width: '10rem', padding: '1rem 2rem 1rem 2rem' }}
-      disabled={phrase == ''}
+      disabled={phrase == '' || currentPrice == null}
       startIcon={<RocketLaunchIcon />}
       onClick={handleMint}>
       Mint
